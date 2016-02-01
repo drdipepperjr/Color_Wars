b89727fbde7436314174be041c7ba3bffb9840a9
athielk@ucsb.umail.edu <Alex Thielk>
2016-02-01 15:49:41 -0800

    game loop running AT

--- Color Wars/src/framework/Game.java
+++ Color Wars/src/framework/Game.java
@@ -1,20 +1,99 @@
 package framework;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Game {
+import javax.swing.*;
+
+
+
+public class Game implements Runnable {
+	JFrame window;
+	JPanel mainMenu;
+	JPanel PlayMenu;
+	JPanel LeaderBoard;
+	private boolean isRunning= false;
+	private Thread thread;
+	
+	private synchronized void start(){
+		if (isRunning==true){
+			return;
+		}
+		isRunning = true;
+		thread= new Thread(this);
+		thread.start();
+	}
+	
+	private synchronized void stop(){
+		if (isRunning==false){
+			return;
+		}
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.exit(1);
+	}
+	
+	public void run(){
+		while (isRunning){
+			// game loop
+			System.err.println("playing");
+		}
+		stop();
+	}
 	
 	public static void main(String args[]){
-		
-		//Window that holds all the JPanels
-		JFrame window = new JFrame();
+		Game game=new Game();
+		game.go();
+
+	}
+	private void go() {
+		this.window = new JFrame();
 		window.setTitle("Color Wars");	
 		window.setSize(1024,768);
 		window.setLocationRelativeTo(null); //centers window on middle of screen
 		window.setResizable(false);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		
+		this.displayMainMenu();
+		
 		window.setVisible(true);
-	
+		//start();
 	}
+	public void displayMainMenu(){
+		mainMenu = new JPanel(); 
+		JButton play = new JButton("Play Color Wars");
+		mainMenu.add(play);
+		play.addActionListener(new playListener());
+	
+		JButton leaderBoard = new JButton("LeaderBoard");
+		mainMenu.add(leaderBoard);
+		leaderBoard.addActionListener(new leaderBoardListener());
+	
+		this.window.getContentPane().add(mainMenu);
+	}	
+	
+	
+	class playListener implements ActionListener{
 
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			System.err.println("play pressed");
+			window.getContentPane().remove(mainMenu);
+			start();
+		}
+	}
+	
+	class leaderBoardListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			System.err.println("LeaderBoard pressed");
+			//game.displayLeaderBoard();
+		}
+	}
 }
