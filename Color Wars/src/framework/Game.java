7f8615349f7df9900a2eefc14e1a6652cc1cc9cc
athielk@ucsb.umail.edu <Alex Thielk>
2016-02-01 22:16:22 -0800

    render() and tick() methods AT

--- Color Wars/src/framework/Game.java
+++ Color Wars/src/framework/Game.java
@@ -11,45 +11,13 @@ public class Game implements Runnable {
 	JFrame window;
 	JPanel mainMenu;
 	JPanel PlayMenu;
-	JPanel LeaderBoard;
+	JPanel LeaderBoard;	
 	private boolean isRunning= false;
 	private Thread thread;
-	
-	private synchronized void start(){
-		if (isRunning==true){
-			return;
-		}
-		isRunning = true;
-		thread= new Thread(this);
-		thread.start();
-	}
-	
-	private synchronized void stop(){
-		if (isRunning==false){
-			return;
-		}
-		isRunning = false;
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.exit(1);
-	}
-	
-	public void run(){
-		while (isRunning){
-			// game loop
-			System.err.println("playing");
-		}
-		stop();
-	}
 	
 	public static void main(String args[]){
 		Game game=new Game();
 		game.go();
-
 	}
 	private void go() {
 		this.window = new JFrame();
@@ -77,13 +45,16 @@ public class Game implements Runnable {
 		this.window.getContentPane().add(mainMenu);
 	}	
 	
-	
 	class playListener implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			System.err.println("play pressed");
 			window.getContentPane().remove(mainMenu);
+			PlayMenu= new Map();
+			window.setVisible(false);
+			window.getContentPane().add(PlayMenu);
+			window.setVisible(true);
 			start();
 		}
 	}
@@ -96,4 +67,69 @@ public class Game implements Runnable {
 			//game.displayLeaderBoard();
 		}
 	}
+	
+	private synchronized void start(){
+		if (isRunning==true){
+			return;
+		}
+		isRunning = true;
+		thread= new Thread(this);
+		thread.start();
+	}
+	
+	private synchronized void stop(){
+		if (isRunning==false){
+			return;
+		}
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.exit(1);
+	}
+	
+	public void run(){
+		long lastTime= System.nanoTime();
+		final double ticks = 60.0;
+		double ns = 1000000000 / ticks;
+		double delta = 0;
+		int updates=0;
+		int frames=0;
+		long timer = System.currentTimeMillis();
+		
+		while (isRunning){
+			long now = System.nanoTime();
+			delta += (now-lastTime)/ns;
+			lastTime=now;
+			if(delta>=1){
+				tick();
+				updates++;
+				delta--;
+			}
+			render();
+			frames++;
+			
+			if(System.currentTimeMillis()-timer > 1000){
+				timer += 1000;
+				System.err.println(updates + " ticks, fps " +frames);
+				updates = 0;
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	private void render() {
+		//for graphic rendering 
+	}
+	private void tick() {
+		//for other stuff like movement 
+		// and stuff we want to go slower
+	}
+	
+
+	
+
 }
