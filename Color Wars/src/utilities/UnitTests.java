d5dda83941bb2d55394d8ba06dc414daeb57ee44
athielk@ucsb.umail.edu <Alex Thielk>
2016-02-12 11:43:03 -0800

    constructor tests

--- Color Wars/src/utilities/UnitTests.java
+++ Color Wars/src/utilities/UnitTests.java
@@ -7,7 +7,7 @@ import java.awt.Color;
 
 import org.junit.Test;
 
-import character.Player;
+import character.*;
 
 public class UnitTests {
 	
@@ -16,5 +16,41 @@ public class UnitTests {
 		assertEquals(10.5,pl.getX(),0.1);
 		assertEquals(17.5,pl.getY(),0.1);
 	}
+	@Test public void testCircleConstructor(){
+		Circle c = new Circle(0,0,Color.BLACK);
+		assertEquals(0,c.getX(),0.1);
+		assertEquals(0,c.getY(),0.1);
+	}
+	@Test public void testTriangleConstructor(){
+		Triangle t = new Triangle(0,0,Color.BLACK);
+		assertEquals(0,t.getX(),0.1);
+		assertEquals(0,t.getY(),0.1);
+	}
 
+	@Test public void testSquareConstructor(){
+		Square s = new Square(0,0,Color.BLACK);
+		assertEquals(0,s.getX(),0.1);
+		assertEquals(0,s.getY(),0.1);
+	}
+	@Test public void testProjectileConstructor(){
+		GameObject t = new Projectile(0,0,0, 0, Color.BLACK);
+		assertEquals(0,t.getX(),0.1);
+		assertEquals(0,t.getY(),0.1);
+	}
+	
+	@Test public void testWaveConstructor(){
+		Wave W = new Wave(5);
+		assertEquals(5,W.size(),0);
+	}
+	
+	@Test public void testProjectilesConstructor(){
+		Projectiles p = new Projectiles();
+		assertEquals(0,p.size(),0);
+		Projectile p1= new Projectile(0,0,0, 0, Color.BLACK);
+		p.add(p1);
+		assertEquals(1,p.size(),0);
+	}
+	
+	
 }
+
