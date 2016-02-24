d96077d0938261c87ca7e7be437feda7b165d034
athielk@ucsb.umail.edu <Alex Thielk>
2016-02-23 18:19:02 -0800

    enemy count done

--- Color Wars/src/framework/Hub.java
+++ Color Wars/src/framework/Hub.java
@@ -35,6 +35,8 @@ public class Hub extends GeneralPathWrapper implements Shape{
 		g2d.setPaint(gp);
 		g2d.draw(healthbar);
 		g2d.fill(healthbar);
+		g2d.setColor(Color.black);
+		g2d.drawString("Remaining Enemies " + wave.numEnemies, 0, 25);
 	}
 
 	public void update(int playerHealth) {
