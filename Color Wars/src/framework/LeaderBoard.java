701a33b41eac28d84ee1094c6cd597d3ef8848d7
athielk@ucsb.umail.edu <Alex Thielk>
2016-03-02 22:04:15 -0800

    leaderboard class implemented just needs a game over screen and such

--- Color Wars/src/framework/LeaderBoard.java
+++ Color Wars/src/framework/LeaderBoard.java
@@ -1,8 +1,14 @@
 package framework;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 
 /*
  * A class that holds the high scores of past players and allows us to add new ones
@@ -10,13 +16,28 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class LeaderBoard extends JPanel{
 	JButton mainMenu;
-	JButton leaderBoard;
 	JFrame window;
+	HighScores highScores;
 	public LeaderBoard(JFrame window){
 		this.window=window;
 		mainMenu = new JButton("Main Menu");
 		this.add(mainMenu);
-		
+		try {
+			FileInputStream fileStream = new FileInputStream("res/HighScores.ser");
+			ObjectInputStream os = new ObjectInputStream(fileStream);
+			Object one = os.readObject();
+			highScores=(HighScores)one;
+			os.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			highScores= new HighScores();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		window.getContentPane().add(this);
 		}
 		
