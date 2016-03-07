bb5337c1f2ce0cd490688c686d89eeb5d52cf9c7
andrevv10 <andrewhuang@umail.ucsb.edu>
2016-03-06 21:50:22 -0800

    Updated leaderboard, looks killa

--- Color Wars/src/framework/LeaderBoard.java
+++ Color Wars/src/framework/LeaderBoard.java
@@ -1,11 +1,13 @@
 package framework;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 
 
 /*
@@ -13,12 +15,15 @@ import javax.swing.JPanel;
  */
 @SuppressWarnings("serial")
 public class LeaderBoard extends JPanel{
-	JButton mainMenu;
+	CustomButton mainMenu;
 	JFrame window;
 	HighScores highScores;
+	ImageIcon img = new ImageIcon("res/leaderboard.jpg");
 	public LeaderBoard(JFrame window, HighScores highScores){
 		this.window=window;
-		mainMenu = new JButton("Main Menu");
+		this.setLayout(null);
+		mainMenu = new CustomButton("Main Menu",317,34);
+		mainMenu.setBounds(340,625,317,34);
 		this.add(mainMenu);
 		this.highScores=highScores;
 		window.getContentPane().add(this);
@@ -26,10 +31,15 @@ public class LeaderBoard extends JPanel{
 	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		g.drawImage(img.getImage(),0,0,window.getWidth(),window.getHeight(),this);
+		g.setColor(Color.white);
+		g.setFont(new Font("TimesRoman",Font.ITALIC,28));
+		g.drawString("Name", 365-5, 261-2);
+		g.drawString("Score", 580-5, 261-2);
+		g.setFont(new Font("TimesRoman",Font.PLAIN,20));
 		for(int i=0;i<10;i++){
-			g.setColor(Color.black);
-			g.drawString("name " + highScores.getName(i), 0, 25*(i+1));
-			g.drawString("Score "+ highScores.getScore(i), 0, 25*(i+1)+10);
+			g.drawString((i+1)+". "+highScores.getName(i), 350-5, 261+29*(i+1));
+			g.drawString(""+ highScores.getScore(i), 580-5, 261+29*(i+1));
 		}
 		
 	}
